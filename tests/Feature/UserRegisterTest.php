3357b33433597c4d9750742a5114754074ac82c1
nom30523 <arikatyo-saiko-@hotmail.co.jp>
2021-07-17 18:48:00 +0900

    circleci setup

--- tests/Feature/UserRegisterTest.php
+++ tests/Feature/UserRegisterTest.php
@@ -17,8 +17,14 @@ class UserRegisterTest extends TestCase
      */
     public function testExample()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $email = 'email@example.com';
+        $this->post(route('register'), [
+            'name' => 'user',
+            'email' => $email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ])
+            ->assertStatus(302);
+        $this->assertDatabaseHas('users', ['email' => $email]);
     }
 }
