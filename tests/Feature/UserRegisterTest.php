ebad1608b0ac6c5851be2c2d8ee1e3e5e5c54731
nom30523 <arikatyo-saiko-@hotmail.co.jp>
2021-07-17 17:23:25 +0900

    php unit setup

--- tests/Feature/UserRegisterTest.php
+++ tests/Feature/UserRegisterTest.php
@@ -3,16 +3,19 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class UserRegisterTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testExample()
     {
         $response = $this->get('/');
 
